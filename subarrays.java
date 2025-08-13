7b3e7d4df8b72ddc5885c47586d89e5969cb43cd
TarangPatel0 <tarangpatel.ds@gmail.com>
2025-08-13 17:54:59 +0530

    Added subarray Program

---
+++ subarrays.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class subarrays {
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 6, 8, 10};
+        for(int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.print("(" + arr[i]);
+                for (int k = i + 1; k <= j; k++) {
+                    System.out.print("," + arr[k]);
+                }
+                System.out.print(") ");
+            }
+            System.out.println();
+        }
+    }
+}
