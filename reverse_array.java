3ebbe5d72ac6a5548888c2f40797ceb8603192f9
TarangPatel0 <tarangpatel.ds@gmail.com>
2025-08-13 17:55:33 +0530

    Added reversing array Program

---
+++ reverse_array.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class reverse_array {
+    public static void reverse(int arr[]) {
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        reverse(arr);
+        for (int i: arr) {
+            System.out.println(i);
+        }
+    }
+}
